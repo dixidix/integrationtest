83c154f37e66314354dbba947f89bb15473a075b
nicolas sigal <nicolas.sigal@globallogic.com>
2016-09-30 16:53:16 -0300

    zxc

--- app/app/app.js
+++ app/app/app.js
@@ -4,7 +4,10 @@
 		.config(['$stateProvider','$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
 			$urlRouterProvider.otherwise("/");
 			$stateProvider
-				.state('home', { url: "/", templateUrl: "./dist/routes/home/home.template.html", data: { title: 'Home', requireAuth: false }, controller: "homeCtrl", controllerAs: "home" });
+				.state('home', { url: "/", templateUrl: "./dist/routes/home/home.template.html", data: { title: 'Home', requireAuth: false }, controller: "homeCtrl", controllerAs: "home" })
+				.state('login', { url: "/login", templateUrl: "./dist/routes/login/login.template.html", data: { title: 'Login', requireAuth: false }, controller: "loginCtrl", controllerAs: "login" })
+				.state('register', { url: "/register", templateUrl: "./dist/routes/register/register.template.html", data: { title: 'Register', requireAuth: false }, controller: "registerCtrl", controllerAs: "register" })
+				.state('users', { url: "/users", templateUrl: "./dist/routes/users/users.template.html", data: { title: 'Users', requireAuth: true }, controller: "usersCtrl", controllerAs: "users" });
 		}])
 		.run(['$rootScope', '$state', '$stateParams', 'authenticationService', function ($rootScope, $state, $stateParams, authenticationService) {
 			$rootScope.$state = $state;
@@ -16,6 +19,9 @@
 			});
 		}]);
 	require('./routes/home/home.js')(angular, app);
+	require('./routes/login/login.js')(angular, app);
+	require('./routes/register/register.js')(angular, app);
+	require('./routes/users/users.js')(angular, app);
 	require('./services/authentication/authentication.js')(angular, app);
 	require('./components/navbar/navbar.js')(angular, app);
 })();
